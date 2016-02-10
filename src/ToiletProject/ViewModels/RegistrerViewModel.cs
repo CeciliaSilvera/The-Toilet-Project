12033322a82da937f4b92f50b83165326ad953e6
Hanna gregersen <hanna_gregersen@hotmail.com>
2016-02-10 13:51:12 +0100

    Hej

---
+++ src/ToiletProject/ViewModels/RegistrerViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToiletProject.ViewModels
+{
+    public class RegistrerViewModel
+    {
+        [Display(Name= "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "E-mail")]
+        public string EMail { get; set; }
+    }
+}
