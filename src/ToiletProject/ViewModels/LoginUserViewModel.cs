0634b141b92da7c4e1b0025e7d275bedeeaba2d7
mohsinrahman <mohsinrahmang@gmail.com>
2016-02-10 14:40:30 +0100

    Attributes

--- src/ToiletProject/ViewModels/LoginUserViewModel.cs
+++ src/ToiletProject/ViewModels/LoginUserViewModel.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ToiletProject.ViewModels
 {
     public class LoginUserViewModel
-    {       
+    {
+        [Display(Name = "User Name")]
+        [Required(ErrorMessage = "Please Enter User Name")]
         public string Name { get; set; }
+
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        [MinLength(8)]
+        [Required(ErrorMessage = "Please Enter Password")]
         public string Password { get; set; }
     }
 }
