2361c42b181aeb92a235a61061b045e4e91ff4bb
Cecilia Silvera <cecilia_silvera@hotmail.com>
2016-02-10 14:44:41 +0100

    added loged in page and jquery

---
+++ src/ToiletProject/ViewModels/LogedInViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToiletProject.ViewModels
+{
+    public class LogedInViewModel
+    {
+        public string UserName { get; set; }
+        public int Id { get; set; }
+    }
+}
